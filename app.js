c2f84fd0a1a77d56867772bd06b3d16966e02b24
Peter Floyd <10535993@mydbs.ie>
2021-03-27 22:08:56 +0000

    Repo Initialised

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const routes = require("./routes");
 
 console.log(process.argv);
 
+//for mongoose connect you can pass in the variable fromt he realease pipeline after npm start//
+//passa rguement for database connection//
 mongoose.connect("mongodb://localhost:27017/ca-db", {useNewUrlParser: true}).then(()=>{
     const app = express();
     app.use(session({
@@ -13,10 +15,13 @@ mongoose.connect("mongodb://localhost:27017/ca-db", {useNewUrlParser: true}).the
         saveUninitialized: false,
         resave: false
     }));
+
+//origin: 'http://localhost:4200'//
     app.use(express.json());
-    app.use(cors({credentials: true, origin: process.argv[2]}));
+//
+    app.use(cors({credentials: true, origin:process.argv[2]}));
     app.use("/api", routes);
-    
+//pass arguement to whitelist the post of the frontend//
     app.listen(3000, ()=>{
         console.log("CA API started on port 3000, test using http://localhost:3000");
     });
