64d892462a4c4debedda73099228008800ec3d31
Peter Floyd <peadarfloyd@gmail.com>
2021-04-14 22:59:00 +0100

    mongo

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ console.log(process.argv);
 //pass an arguement for database connection//
 ////remove --- mongodb://localhost:27017/ca-db
 //mongodb://pf-10535993-mongo-loadbalancer-staging/database
+//$MONGOURL
 
-mongoose.connect("$MONGOURL", {useNewUrlParser: true}).then(()=>{
+mongoose.connect("mongodb://pf-10535993-mongo-loadbalancer-staging/database", {useNewUrlParser: true}).then(()=>{
     const app = express();
     app.use(session({
         secret : "caAPISecret",
