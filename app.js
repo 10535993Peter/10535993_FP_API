05db42b8eae02fa3b8e06f6034120c18d2532afa
Peter Floyd <peadarfloyd@gmail.com>
2021-04-14 22:43:04 +0100

    p

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const routes = require("./routes");
 
 console.log(process.argv);
 
-//for mongoose connect you can pass in the variable from the realease pipeline after npm start//
+//for mongoose connect you can pass in the variable from the release pipeline after npm start//
 //pass an arguement for database connection//
 ////remove --- mongodb://localhost:27017/ca-db
 //mongodb://pf-10535993-mongo-loadbalancer-staging/database
 
-mongoose.connect("mongodb://pf-10535993-mongo-loadbalancer-staging/database", {useNewUrlParser: true}).then(()=>{
+mongoose.connect("$MONGOURL", {useNewUrlParser: true}).then(()=>{
     const app = express();
     app.use(session({
         secret : "caAPISecret",
@@ -22,7 +22,7 @@ mongoose.connect("mongodb://pf-10535993-mongo-loadbalancer-staging/database", {u
 //origin: 'http://localhost:4200'//
     app.use(express.json());
 // update below with app.use(cors({credentials: true, origin:process.argv[2], process.argv[3]}));
-    app.use(cors({credentials: true, origin:process.argv[2],}));
+    app.use(cors({credentials: true, origin:process.argv[2], origin:process.argv[3]}));
     app.use("/api", routes);
 //pass arguement to whitelist the port of the frontend//
 //http://40.67.253.31/
