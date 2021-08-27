8c02c53110bf37a05b9bbdfec03e036c2f12c33c
Peter <peadarfloyd@gmail.com>
2021-08-27 11:43:21 +0100

    updated app.js

--- app.js
+++ app.js
@@ -18,31 +18,10 @@ mongoose.connect(mongo, {useNewUrlParser: true}).then(()=>{
 
 ////origin: 'http://localhost:4200'//
     app.use(express.json());
-    app.use(cors({origin: [process.argv[2], process.argv[3]]}));
+    app.use(cors({credentials: true, origin:[process.argv[2], process.argv[3]]}));
     app.use("/api", routes);
     app.listen(3000, ()=>{
         console.log("CA API started on port 3000, test using http://localhost:3000/");
     });
 });
 
-// console.log(process.argv);
-// ////for mongoose connect you can pass in the variable from the release pipeline after npm start//
-// ////pass an arguement for database connection//
-
-// mongoose.connect("mongodb://localhost:27017/ca-db", {useNewUrlParser: true}).then(()=>{
-//     const app = express();
-//     app.use(session({
-//         secret : "caAPISecret",
-//         saveUninitialized: false,
-//         resave: false
-//     }));
-
-// ////origin: 'http://localhost:4200'//
-//     app.use(express.json());
-//     app.use(cors({credentials: true, origin:process.argv[2]}));
-//     app.use("/api", routes);
-
-//     app.listen(3000, ()=>{
-//         console.log("CA API started on port 3000, test using http://localhost:3000/");
-//     });
-// });
