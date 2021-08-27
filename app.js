6787c375d2be4157d2ccc9c2f1d82a1f4e4ec60c
Peter <peadarfloyd@gmail.com>
2021-08-27 11:20:30 +0100

    updated app.js argv

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
+console.log(process.argv);
+var mongo = process.argv[4];
+// launch api locally by passing in all arguements: node app.js http://localhost:4200 http://localhost:3000 mongodb://localhost:27017/ca-db
+
+mongoose.connect(mongo, {useNewUrlParser: true}).then(()=>{
+    const app = express();
+    app.use(session({
+        secret : "apisecret",
+        saveUninitialized: false,
+        resave: false
+    }));
+
+////origin: 'http://localhost:4200'//
+    app.use(express.json());
+    app.use(cors({origin: [process.argv[2], process.argv[3]]}));
+    app.use("/api", routes);
+    app.listen(3000, ()=>{
+        console.log("CA API started on port 3000, test using http://localhost:3000/");
+    });
+});
+
 // console.log(process.argv);
 // ////for mongoose connect you can pass in the variable from the release pipeline after npm start//
 // ////pass an arguement for database connection//
@@ -25,25 +46,3 @@ const routes = require("./routes");
 //         console.log("CA API started on port 3000, test using http://localhost:3000/");
 //     });
 // });
-
-console.log(process.argv);
-var mongo = process.argv[4];
-// launch api locally by passing in all arguements: node app.js http://localhost:4200 http://localhost:3000 mongodb://localhost:27017/ca-db
-
-mongoose.connect(mongo, {useNewUrlParser: true}).then(()=>{
-    const app = express();
-    app.use(session({
-        secret : "apisecret",
-        saveUninitialized: false,
-        resave: false
-    }));
-
-////origin: 'http://localhost:4200'//
-    app.use(express.json());
-    app.use(cors({credentials: true, origin:process.argv[2]}, process.argv[3]));
-    app.use("/api", routes);
-    app.listen(3000, ()=>{
-        console.log("CA API started on port 3000, test using http://localhost:3000/");
-    });
-});
-
